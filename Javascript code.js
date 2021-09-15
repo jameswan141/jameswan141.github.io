263241f11e899f4eaa62ba2f0ea464eb543341cd
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-09-14 22:04:18 -0400

    commit on 'main'

--- Javascript code.js
+++ Javascript code.js
@@ -9,65 +9,46 @@ function city() {
 
 
 
+document.getElementById("demo").innerHTML = "Hello World!";
+
+
+
+document.getElementById('myImage').src="picture.gif"
+
+
+
+document.getElementById("demo").style.fontSize = "30px";
+
+
+
+document.getElementById("demo").style.display = "none";
+
+
+
+document.getElementById("demo").style.display = "block";
+
+
+
+<script>
+document.getElementById("demo").innerHTML = "The second use of script.";
+</script>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Number Guessing Game</title>
-
-	<style>
-		html {
-	font-family: sans-serif;
-	}
-	
-		body {
-	width: 50%;
-	max-width: 75px;
-	min-width: 450px;
-	margin: 0 auto;
-	}
-	</style>
-</head>
-
-<body>
-<h1>Guessing Game</h1>
-
-<p>I have selected a random number between 1 - 10.
-Try to guess it.</p>
-
-<div class="form">
-	<label for="Field">Enter a guess: </label>
-	<input type = "text" id = "Field" class = "Field">
-	<input type = "submit" value = "Submit"
-		class = "guessSubmit" id = "submitguess">
-</div>
-
-<script type = "text/javascript">
-
-	var y = Math.floor(Math.random() * 10 + 1);
-	
-	var guess = 1;
-	
-	document.getElementById("submitguess").onclick = function(){
-	
-var x = document.getElementById("Field").value;
-
-if(x == y)
-{	
-	alert("Congratulations! You guessed the right number in "
-			+ guess + " guesses!");
-}
-else if(x > y) 
-{	
-	guess++;
-	alert("Sorry! The number is smaller.");
-}
-else
-{
-	guess++;
-	alert("Sorry! The number is greater.")
-}
+<script>
+function changeParagraph() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 </script>
+</head>
+<body>
+<h1>Demo JavaScript in head section.</h1>
+
+<p id="demo">A paragraph</p>
+<button type="button" onclick="changeParagraph()">It works</button>
+
 </body>
-</html>					
+</html>
