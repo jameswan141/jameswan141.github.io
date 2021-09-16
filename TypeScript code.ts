d30f804ced7bc7e45742174067e5b0d48c8a4f4f
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-09-16 09:47:14 -0400

    Update TypeScript code.ts

--- TypeScript code.ts
+++ TypeScript code.ts
@@ -51,3 +51,20 @@ const user: User = {
   name: "James",
   id: 0,
 };
+
+interface User {
+  name: string;
+  id: number;
+}
+ 
+class UserAccount {
+  name: string;
+  id: number;
+ 
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+ 
+const user: User = new UserAccount("James", 0);
